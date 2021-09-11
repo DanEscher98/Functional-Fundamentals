e759227d253eff55055b45cf8159ebe7b004b64f
DanEscher98 <danyiel5978@gmail.com>
2021-09-11 01:44:32 -0500

    hello-world getValidFile function

--- Techniques/Pure-IO/hello-world.hs
+++ Techniques/Pure-IO/hello-world.hs
@@ -1,12 +1,31 @@
 module Main (main) where
+import           Data.Char
+import           System.Directory
+import           System.IO
+import           System.IO.Unsafe
 
 main :: IO ()
-main = do {
-          print ([x | x <- [0..10], even x]);
-          putStrLn "Hello World";
-          putStrLn "Bye";
-          return ()
-          }
+main = do
+    s <- prompt "Set your name: "
+    putStrLn ("Hello " ++ s)
+    ifile <- getValidFile "Input file: "
+    ofile <- getValidFile "Output file: "
+    s <- readFile ifile
+    writeFile ofile (map toLower (filter (not . isNumber) s))
+    putStrLn "Goodbye!"
+
+
+getValidFile :: String -> IO String
+getValidFile text
+    | unsafePerformIO . doesFileExist $ s = return s
+    | otherwise = getValidFile "Try again: "
+        where s = unsafePerformIO . prompt $ text
+
+prompt :: String -> IO String
+prompt text = do
+    putStr text
+    hFlush stdout -- Buffering cuestion
+    getLine
 
 tomaLinea :: IO String
 tomaLinea = getChar >>=
