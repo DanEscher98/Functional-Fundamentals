ccd033707f56a5c2aea577ea71065f4a60bc9489
Danyiel Colin <danyiel5978@gmail.com>
2022-06-21 01:47:11 -0500

    new file:   Techniques/MultiComputing/Dockerfile modified:   Techniques/MultiComputing/Parallel_Basics.hs

--- Techniques/MultiComputing/Parallel_Basics.hs
+++ Techniques/MultiComputing/Parallel_Basics.hs
@@ -2,6 +2,16 @@ module Main (main) where
 
 import Control.Parallel.Strategies
 
+fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 2) + fib (n - 1)
+
 main :: IO ()
 main = do
-    putStrLn "Hi :)"
+    print $! runEval $ do
+        a <- rpar $ fib 32
+        b <- rpar $ fib 12
+        rseq a
+        rseq b
+        return (a, b)
