e41477d3621bbdde0df18f0f0f8527bd45434f2b
Danyiel Colin <danyiel5978@gmail.com>
2023-06-20 05:03:46 -0600

    modified:   .gitignore 	deleted:    CategoryLambda/BasicOperators.md 	deleted:    CategoryLambda/Course/ctext.md 	deleted:    CategoryLambda/Course/temario.md 	deleted:    CategoryLambda/Functors.md 	deleted:    CategoryLambda/HigerKindedTypes.md 	deleted:    CategoryLambda/Monad.md 	deleted:    CategoryLambda/basicFormulas.md 	deleted:    CategoryLambda/categories.md 	deleted:    CategoryLambda/category_notes.md 	deleted:    CategoryLambda/definitions.md 	deleted:    CategoryLambda/lambda.md 	deleted:    CategoryLambda/syntacticSugar.md 	deleted:    CategoryLambda/thougthsMonads.md 	deleted:    Combinatorics/combinatoria-1.md 	new file:   Monads/ErrorEncapsulated.hs 	new file:   playhsom/.ghci 	new file:   playhsom/.gitignore 	new file:   playhsom/CHANGELOG.md 	new file:   playhsom/LICENSE 	new file:   playhsom/README.md 	new file:   playhsom/Setup.hs 	new file:   playhsom/app/Main.hs 	new file:   playhsom/app/RandomMusic.hs 	new file:   playhsom/app/SimpleMusic.hs 	new file:   playhsom/dist-newstyle/cache/config 	new file:   playhsom/package.yaml 	new file:   playhsom/src/Lib.hs 	new file:   playhsom/stack.yaml 	new file:   playhsom/test/Spec.hs

--- CategoryLambda/basicFormulas.md
+++
@@ -1,8 +0,0 @@
-# Definiciones básicas
-
-## Lógica
-
-```lambdaCalculus
-F ≡ (λx.x)
-
-```
---
+++ playhsom/app/RandomMusic.hs
@@ -0,0 +1,51 @@
+module RandomMusic where
+import Euterpea
+import System.Random (mkStdGen, random, StdGen, randomR)
+
+randInts :: Int -> [Int]
+randInts seed = recInts (mkStdGen seed) where
+  recInts g = let (i, g') = random g in i : recInts g'
+
+randIntsRange :: (Int, Int) -> Int -> [Int]
+randIntsRange (lower, upper) =
+  map (\i -> (i `mod` (upper - lower)) + lower) . randInts
+
+melGen :: Int -> Music (Pitch, Volume)
+melGen s =
+  let pitches = map pitch $ randIntsRange (30, 80) s
+      vols = randIntsRange (40, 100) (s+1)
+  in line $ zipWith (curry (note sn)) pitches vols
+
+somethingWeird = 
+  let part1 = instrument Lead6Voice $ dim $ rit $ cut 6 $ melGen 345
+      part2 = instrument Marimba $ cut 4 $ melGen 234
+      part3 = instrument TubularBells $ cre $ acc $ cut 8 $ melGen 789
+  in  chord [part1, part2, part3] where
+    rit = Modify (Phrase [Tmp $ Ritardando 0.5])
+    acc = Modify (Phrase [Tmp $ Accelerando 0.5])
+    dim = Modify (Phrase [Dyn $ Diminuendo 0.5])
+    cre = Modify (Phrase [Dyn $ Crescendo 0.5])
+
+
+choose :: [a] -> StdGen -> (a, StdGen)
+choose [] g = error "Nothing to choose from!"
+choose xs g = 
+  let (i, g') = random g
+  in  (xs !! (i `mod` length xs), g')
+
+randomMel :: [AbsPitch] -> [Dur] -> Volume -> StdGen -> Music (AbsPitch, Volume)
+randomMel pitches durs thresh g0 = 
+  let (p, g1) = choose pitches g0 
+      (d, g2) = choose durs g1 
+      (v, g3) = randomR (0,127) g2
+      x = if v < thresh then rest d else note d (p,v) 
+  in  x :+: randomMel pitches durs thresh g3
+
+pitches1, pitches2 :: [AbsPitch]
+pitches1 = [60,62,63,65,67,68,70,72,74,75,77,79] -- C-minor
+pitches2 = [36,43,46,48] -- also C-minor (root, 5th, 7th, root)
+
+mel1, mel2, duet :: Music (AbsPitch, Volume)
+mel1 = randomMel pitches1 [qn,en,en,en] 40 (mkStdGen 500)
+mel2 = randomMel pitches2 [hn,qn,qn,qn] 20 (mkStdGen 501)
+duet = tempo 1.75 (instrument Celesta mel1 :=: instrument AcousticBass mel2)
